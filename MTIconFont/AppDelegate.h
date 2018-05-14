dbcdf5b1e5f1e6041b318d2f38f74a4a23c8b842
范冬冬 <fdd@meitu.com>
2018-05-14 19:36:08 +0800

    first commit

---
+++ MTIconFont/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTIconFont
+//
+//  Created by 范冬冬 on 2018/5/14.
+//  Copyright © 2018年 meitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
